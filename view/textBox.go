d49d64c55d83c450c98baa7ba86413afd602d8a8
Quinn Pollock <contact@quinnpollock.net>
2020-07-08 02:31:12 -0400

    add cursor

--- view/textBox.go
+++ view/textBox.go
@@ -24,12 +24,28 @@ func NewTextBox() *TextBox {
 	}
 }
 
+var cur = Cell{
+	Rune: ' ',
+	Style: Style{
+		Fg: ColorBlack,
+		Bg: ColorWhite,
+	},
+}
+
 func (self *TextBox) Draw(buf *Buffer) {
 	self.Block.Draw(buf)
 	cells := ParseStyles(self.Buffer, self.TextStyle)
 
 	for i := 0; i < len(cells); i++ {
+		if i == self.cursor {
+			cells[i].Style.Bg = ColorWhite
+			cells[i].Style.Fg = ColorBlack
+		}
 		buf.SetCell(cells[i], image.Pt(i, 0).Add(self.Inner.Min))
+	}
+	if self.cursor == self.length {
+		buf.SetCell(cur, image.Pt(self.length, 0).Add(self.Inner.Min))
+
 	}
 }
 
